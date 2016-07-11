72c530b13e037ec3575003b898b1026cd733bd74
Abhishek Gautam <agautam2@buffalo.edu>
2016-07-11 17:32:04 -0400

    shift and unshift

--- arrayoperations.pl
+++ arrayoperations.pl
@@ -1,10 +1,27 @@
 #!/usr/bin/perl
-#use of array operations like: reverse
+#use of array operations like: reverse, shift and unshift
 use warnings;
 my @count = (1..5);
+print "array contains: @count\n";
 for (reverse(@count))
 {
 print "$_...\n";
 sleep 1;
 }
 print "BLAST OFF!\n";
+sleep 1;
+print "adding 100 to beginning of the array\n";
+sleep 1;
+unshift @count, 100;
+print "adding 200 to beginning of the array\n";
+sleep 1;
+unshift @count, 200;
+sleep 1;
+print "Now array contains: @count\n";
+sleep 1;
+print "shifting array. shifting items to the right\n";
+sleep 1;
+shift @count;
+print "Now array contains: @count\n";
+sleep 1;
+print "that's all folks!";
